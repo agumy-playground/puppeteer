ab30c9d92e1526bfe8b9d96a697b6c000297e367
agumy <nov13sh@gmail.com>
2021-05-01 16:55:44 +0900

    add translation using deepL

--- src/example.js
+++ src/example.js
@@ -1,10 +1,20 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--lang=ja"],
+  });
   const page = await browser.newPage();
-  await page.goto("https://google.com");
-  await page.screenshot({ path: "exmaple.png" });
+  await page.goto("https://www.deepl.com/translator");
+  const textarea = await page.$(".lmt__source_textarea");
+  await page.evaluate((element) => {
+    args: ["--lang=ja"];
+    element.value = "Hello";
+    element.dispatchEvent(new KeyboardEvent("input"));
+  }, textarea);
 
-  await browser.close();
+  setTimeout(async () => {
+    await page.screenshot({ path: "deepl.png" });
+    await browser.close();
+  }, 3000);
 })();
